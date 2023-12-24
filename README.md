34fa95a50a895bff530b64e1545cdb47ea9779fc
Leo Wu <leo.wu.kw@gmail.com>
2023-12-25 01:35:48 +0800

    init GoLang logger

---
+++ README.md
@@ -0,0 +1,13 @@
+# go-logger
+
+Simple logger with color.
+
+```
+logger.Debug("") // grey
+
+logger.Info("") // grey
+
+logger.Warn("") // yellow
+
+logger.Error("") // red
+```
---
+++ logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors: true,
+	})
+}
+
+func ansi(colorString string) func(...interface{}) string {
+	return func(args ...interface{}) string {
+		return fmt.Sprintf(colorString, fmt.Sprint(args...))
+	}
+}
+
+var (
+	grey   = ansi("\033[1;38m%s\033[0m")
+	green  = ansi("\033[1;32m%s\033[0m")
+	yellow = ansi("\033[1;33m%s\033[0m")
+	red    = ansi("\033[1;31m%s\033[0m")
+)
+
+//goland:noinspection GoUnusedExportedFunction
+func Debug(msg string) {
+	logrus.Debug(grey(msg))
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func Info(msg string) {
+	logrus.Info(green(msg))
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func Warn(msg string) {
+	logrus.Warn(yellow(msg))
+}
+
+//goland:noinspection GoUnusedExportedFunction
+func Error(msg string) {
+	logrus.Error(red(msg))
+}
